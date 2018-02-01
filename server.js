c2381911cd98e2878d61ad00047e889800fb77bf
kaushal1214 <bonjourandoird.hotmail.com>
2018-02-02 00:14:42 +0530

    Socket.io added

--- server.js
+++ server.js
@@ -8,58 +8,30 @@ var config = require('./server/configure');
 var mongoose = require('mongoose');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+/*
+var LCD = require('lcd'),
+	lcd =  new LCD({
+		rs: 3,
+		e: 2,
+		data: [6,13,16,19],
+		cols: 20,
+		row: 4
+	});
 
+var READY = false;
 
-// Twilio Credentials
-const accountSid = 'AC55b6fbdd75183c5a57d5353e04f26b32';
-const authToken = '7fa59fe3498bd1887b15f4e08898b687';
-
-// require the Twilio module and create a REST client
-const client = require('twilio')(accountSid, authToken);
-
-
-
-
+lcd.on('ready',function(){
+	READY = true;
+}); */
 app.set('port',process.env.PORT || 3300);
 app.set('views',__dirname + '/views');
 app = config(app);
 
-mongoose.connect("mongodb://localhost/iot");
-mongoose.connection.on('open',function(){
-	console.log("Mongoose Connected");
-});
-
-io.on('connection', function(){
-	console.log('A new Client connected');
-});
-
 http.listen(3300,function(){
 console.log('Server up and running at 3300 port');
-
 });
-
-var twilioMsg = function(){
-
-        client.messages
-                  .create({
-                    to: '+917679480088',
-                    from: '+16154423004',
-                    body: 'The Artefact has been misplaced.',
-          })
-        .then((message) => console.log(message.sid));
-
-
-}
-
-app.post('/artefactUpdate', function(req,res){
-	console.log('ID:'+req.body.id);
-	io.emit('State Changed', { id: req.body.id,
-				   state: req.body.state}
-	);
-
-	twilioMsg();
-
-	//Server reply
-	res.json({Status:'Ok'});
+io.on('connection',function(client){
+	client.on('Message',function(data){
+		console.log(data.data);
+	});
 });
-
